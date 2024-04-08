10d071a38884041ba3b27bf725b6e1928e2a0f0d
EnzoFerrodri <enzoferrodrigues@gmail.com>
2024-04-08 15:45:15 -0300

    comitado

--- stm32f411-blackpill/src/main.c
+++ stm32f411-blackpill/src/main.c
@@ -21,9 +21,14 @@
 
 #define STM32_RCC_BASE       0x40023800     /* 0x40023800-0x40023bff: Reset and Clock control RCC */
 
+
 /* GPIOC Base Addresses ******************************************************/
 
 #define STM32_GPIOC_BASE     0x40020800     /* 0x48000800-0x48000bff: GPIO Port C */
+
+/* GPIOA Base Addresses ******************************************************/
+#define STM32_GPIOA_BASE     0x40020000 //0x48000000-0x480003ff
+
 
 /* Register Offsets *********************************************************/
 
@@ -35,6 +40,7 @@
 #define STM32_GPIO_ODR_OFFSET     0x0014  /* GPIO port output data register */
 #define STM32_GPIO_BSRR_OFFSET    0x0018  /* GPIO port bit set/reset register */
 
+#define STM32_GPIO_IDR_OFFSET 0X0010
 
 /* Register Addresses *******************************************************/
 
@@ -46,9 +52,16 @@
 #define STM32_GPIOC_ODR          (STM32_GPIOC_BASE+STM32_GPIO_ODR_OFFSET)
 #define STM32_GPIOC_BSRR         (STM32_GPIOC_BASE+STM32_GPIO_BSRR_OFFSET)
 
+#define STM32_GPIOA_MODER        (STM32_GPIOA_BASE + STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOA_OTYPER       (STM32_GPIOA_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOA_PUPDR        (STM32_GPIOA_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOA_IDR          (STM32_GPIOA_BASE+STM32_GPIO_IDR_OFFSET)
+#define STM32_GPIOA_BSRR         (STM32_GPIOA_BASE+STM32_GPIO_BSRR_OFFSET)
+
 /* AHB1 Peripheral Clock enable register */
 
 #define RCC_AHB1ENR_GPIOCEN      (1 << 2)  /* Bit 2:  IO port C clock enable */
+#define RCC_AHB1ENR_GPIOAEN      (1 << 0)
 
 /* GPIO port mode register */
 
@@ -86,25 +99,6 @@
 
 #define LED_DELAY  100000
 
-/****************************************************************************
- * Private Types
- ****************************************************************************/
-
-/****************************************************************************
- * Private Function Prototypes
- ****************************************************************************/
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
-
- /****************************************************************************
- * Private Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
 
 int main(int argc, char *argv[])
 {
@@ -115,14 +109,19 @@ int main(int argc, char *argv[])
 
   uint32_t *pRCC_AHB1ENR  = (uint32_t *)STM32_RCC_AHB1ENR;
   uint32_t *pGPIOC_MODER  = (uint32_t *)STM32_GPIOC_MODER;
+  uint32_t *pGPIOA_MODER  = (uint32_t *)STM32_GPIOA_MODER;
   uint32_t *pGPIOC_OTYPER = (uint32_t *)STM32_GPIOC_OTYPER;
+  uint32_t *pGPIOA_OTYPER = (uint32_t *)STM32_GPIOA_OTYPER;
   uint32_t *pGPIOC_PUPDR  = (uint32_t *)STM32_GPIOC_PUPDR;
+  uint32_t *pGPIOA_PUPDR  = (uint32_t *)STM32_GPIOA_PUPDR;
   uint32_t *pGPIOC_BSRR   = (uint32_t *)STM32_GPIOC_BSRR;
+  uint32_t *pGPIOC_BSRR   = (uint32_t *)STM32_GPIOA_BSRR;
 
-  /* Habilita clock GPIOC */
+  /* Habilita clock GPIOC e gpioA*/
 
   reg  = *pRCC_AHB1ENR;
   reg |= RCC_AHB1ENR_GPIOCEN;
+  reg |= RCC_AHB1ENR_GPIOAEN;
   *pRCC_AHB1ENR = reg;
 
   /* Configura PC13 como saida pull-up off e pull-down off */
@@ -142,20 +141,28 @@ int main(int argc, char *argv[])
   reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(13));
   *pGPIOC_PUPDR = reg;
 
-  while(1)
-    {
-      /* Liga LED */
+  // Configura PA0 como entrada com pull-up
+  reg = *pGPIOA_MODER;
+  reg &= ~GPIO_MODER_MASK(0);
+  reg |= (GPIO_MODER_INPUT << GPIO_MODER_SHIFT(0));
+  *pGPIOA_MODER = reg;
 
-      *pGPIOC_BSRR = GPIO_BSRR_RESET(13);
-      for (i = 0; i < LED_DELAY; i++);
+  reg = *pGPIOA_PUPDR;
+  reg &= ~GPIO_PUPDR_MASK(0);
+  reg |= (GPIO_PUPDR_PULLUP << GPIO_PUPDR_SHIFT(0));
+  *pGPIOA_PUPDR = reg;
 
-      /* Desliga LED */
-
-      *pGPIOC_BSRR = GPIO_BSRR_SET(13);
-      for (i = 0; i < LED_DELAY; i++);
+   while(1) {
+        // Verifica se o botao foi pressionado (nvel logico baixo)
+        if (not(STM32_GPIOA_IDR & (1 << 0))) {
+            // liga led
+            *pGPIOC_BSRR = GPIO_BSRR_RESET(13);
+        } else {
+            // desliga led
+            *pGPIOC_BSRR = GPIO_BSRR_SET(13);
+        }
     }
 
-  /* Nunca deveria chegar aqui */
-
-  return EXIT_FAILURE;
+    /* Nunca deveria chegar aqui */
+    return EXIT_FAILURE;
 }
